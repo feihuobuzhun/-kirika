724b72bd038009f92a3aea0cad1f16b565b3d873
Stephen Zhou <hi@hyoban.cc>
2023-05-23 21:35:54 +0800

    feat: support google keep

--- packages/web/app/api/route.ts
+++ packages/web/app/api/route.ts
@@ -1,4 +1,8 @@
-import { readMemosFromOpenAPI } from "kirika"
+import {
+  readGoogleKeepTakeout,
+  readMemosFromOpenAPI,
+  writeMemosWithResources,
+} from "kirika"
 
 import { zipMemos } from "@/lib/convert/zip"
 
@@ -23,6 +27,19 @@ export async function POST(request: Request) {
         },
       })
     } catch (error) {
+      console.error(error)
+      return new Response("Failed to convert", { status: 500 })
+    }
+  } else if (from === "google-keep" && to === "memos") {
+    try {
+      const openAPI = searchParams.get("openAPI") as string
+      // read google keep takeout zip from request body
+      const zip = await request.arrayBuffer()
+      const memosWithResource = await readGoogleKeepTakeout(zip)
+      await writeMemosWithResources(openAPI, memosWithResource)
+      return new Response("Success", { status: 200 })
+    } catch (error) {
+      console.error(error)
       return new Response("Failed to convert", { status: 500 })
     }
   }
